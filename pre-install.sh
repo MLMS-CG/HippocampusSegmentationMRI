7d676d9bda6df4a9fb1a72737986db35ab7c8fb6
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-03-08 09:50:07 +0100

    feat: add build-essential dependency

--- pre-install.sh
+++ pre-install.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # Install build dependencies
-sudo apt install cmake
+sudo apt install build-essential cmake
 
 # Bind all deprecated git protocol url to https protocol
 git config --global url.https://github.com/.insteadOf git://github.com/
