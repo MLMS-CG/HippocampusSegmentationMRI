bde9128c1e1c343f259396915152100e4700eeda
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-03-08 09:33:03 +0100

    feat: add a pre-install script

---
+++ pre-install.sh
@@ -0,0 +1,5 @@
+# Install build dependencies
+sudo apt install cmake
+
+# Bind all deprecated git protocol url to https protocol
+git config --global url.https://github.com/.insteadOf git://github.com/
