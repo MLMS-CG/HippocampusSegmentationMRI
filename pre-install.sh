23c41ad8de8f87452c34046d89fba7e497edb6ab
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-03-11 10:37:02 +0100

    feat: complete pre-install script

--- pre-install.sh
+++ pre-install.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+# Bind all deprecated git protocol url to https protocol
+git config --global url.https://github.com/.insteadOf git://github.com/
+
 # Install build dependencies
 sudo apt update
-sudo apt install build-essential cmake
+sudo apt install build-essential cmake flex
 
 # Install old GCC 8.5.0
+sudo update-alternatives --remove gcc "${PWD}/gcc/xgcc"
+sudo update-alternatives --remove g++ "${PWD}/gcc/xg++"
 rm -rf ./gcc-source ./gcc-build ./gcc
+
+## Search for GCC source
 git clone --depth 1 --branch releases/gcc-8.5.0 https://github.com/gcc-mirror/gcc.git gcc-source
-
-cd ./gcc-source
-./contrib/download_prerequisites
-
+cd ./gcc-source && ./contrib/download_prerequisites
 unset C_INCLUDE_PATH CPLUS_INCLUDE_PATH CFLAGS CXXFLAGS
+cd ../
 
-mkdir ../gcc-build
-cd ../gcc-build
-
+## Build GCC
+mkdir ./gcc-build && cd ./gcc-build
 ../gcc-source/configure \
-    --prefix=/usr/gcc-trunk \
+    --prefix=/gcc \
     --enable-languages=c,c++ \
     --disable-libquadmath \
     --disable-libquadmath-support \
@@ -28,8 +32,14 @@ cd ../gcc-build
 
 make -j4
 
-mkdir ../gcc
-make DESTDIR="../gcc" install
+make DESTDIR="${PWD}" install
+mv ./gcc ../
 
-# Bind all deprecated git protocol url to https protocol
-git config --global url.https://github.com/.insteadOf git://github.com/
+sudo apt remove flex
+sudo apt autoremove
+
+cd ../
+
+## Set old GCC
+sudo update-alternatives --install /usr/bin/gcc gcc "${PWD}/gcc/xgcc" 1
+sudo update-alternatives --install /usr/bin/g++ g++ "${PWD}/gcc/xg++" 1
